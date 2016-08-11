3a693ddad814480bc23ebece49e42727cf562e7b
Peter Defebvre <macpie59@gmail.com>
2016-08-11 10:01:46 -0700

    Add routing

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react'
 import {
     render
 } from 'react-dom'
+import {
+    Router,
+    Route,
+    hashHistory
+} from 'react-router'
 import GifApp from './containers/GifApp';
 import * as GifAPI from './apis/GifAPI';
 
 GifAPI.mock();
 GifAPI.loadData();
 
-render(
-    <GifApp />,
-    document.getElementById('root')
+const router = (
+    <Router history={hashHistory}>
+        <Route path="/" component={GifApp}>
+            <Route path="*" component={GifApp} />
+        </Route>
+    </Router>
 );
+
+render(router, document.getElementById('root'));

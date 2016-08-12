9074ad8a8d21262f46fe198bdcf987e001f81277
Peter Defebvre <macpie59@gmail.com>
2016-08-12 09:09:34 -0700

    Add promise and config

--- src/apis/GiphyAPI.js
+++ src/apis/GiphyAPI.js
@@ -1,17 +1,20 @@
 import request from 'superagent';
-
-const API_KEY = '5xtDarp1MN8nPb4X9vi';
+import Promise from 'promise';
+import {GIPHY_API_KEY} from '../config';
 
 export const search = (req) => {
-    request
-        .get('http://api.giphy.com/v1/gifs/search')
-        .query({
-            api_key: API_KEY,
-            q: req.query,
-            limit: req.limit || 10,
-            offset: req.offset || 0
-        })
-        .end(function(err, res) {
-            console.log(err, res);
-        });
+    return new Promise((resolve, reject) => {
+        request
+            .get('http://api.giphy.com/v1/gifs/search')
+            .query({
+                api_key: GIPHY_API_KEY,
+                q: req.query,
+                limit: req.limit || 10,
+                offset: req.offset || 0
+            })
+            .end((err, res) => {
+                if (err) reject(err);
+                else resolve(res.body);
+            });
+    });
 };

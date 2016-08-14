8fff4195c24bd742024c975852e0c6ebe4361cb2
Peter Defebvre <macpie59@gmail.com>
2016-08-14 01:06:28 -0700

    Add more gifs to mock

--- src/apis/GifAPI.js
+++ src/apis/GifAPI.js
@@ -44,7 +44,22 @@ export const mock = () => {
     let data = [
         {
             id: '1',
-            name: 'Au bucher!',
+            name: 'Test 1',
+            url: 'http://i.imgur.com/y5x7n4m.gif'
+        },
+        {
+            id: '2',
+            name: 'Test 22222222222222222222',
+            url: 'http://i.imgur.com/BjJyQIc.gif'
+        },
+        {
+            id: '3',
+            name: 'Test 3',
+            url: 'http://i.imgur.com/AKUf1yY.gif'
+        },
+        {
+            id: '5',
+            name: 'Test 5',
             url: 'http://i.imgur.com/y5x7n4m.gif'
         }
     ];

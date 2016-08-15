e2850cbd45d1ebd6014e519b63c0d529e7a7e927
Peter Defebvre <macpie59@gmail.com>
2016-08-15 10:21:02 -0700

    Add AddModal in filter form

--- src/components/gif/GifFilter.js
+++ src/components/gif/GifFilter.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import GifAddModal from '../common/GifAddModal'
 import * as GifActions from '../../actions/GifActions';
-
 
 class GifFilter extends React.Component {
     constructor(props) {
         super(props);
 
         this.handleFilterChange = this.handleFilterChange.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
     }
     handleFilterChange(e) {
         GifActions.filter(e.target.value);
+    }
+    handleAdd() {
+        $('#GifAddModal').modal('show');
     }
     render() {
         return (
             <div id="filter" className="row">
                 <div className="col-xs-6 col-xs-offset-3">
-                    <input type="text" name="filter" className="form-control" placeholder="Filter by name" onChange={this.handleFilterChange} />
+                    <div className="input-group">
+                        <input type="text" name="filter" className="form-control" placeholder="Filter by name" onChange={this.handleFilterChange} />
+                        <div className="input-group-btn">
+                            <button className="btn btn-default" onClick={this.handleAdd}>
+                                <i className="fa fa-plus" aria-hidden="true"></i>
+                            </button>
+                        </div>
+                    </div>
                 </div>
+                <GifAddModal />
             </div>
         );
     }

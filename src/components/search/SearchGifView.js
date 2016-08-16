004e992a918d126100fec0ceb72d2ac83fd8a3b8
Peter Defebvre <macpie59@gmail.com>
2016-08-16 16:52:19 -0700

    Add mouseOver events

--- src/components/search/SearchGifView.js
+++ src/components/search/SearchGifView.js
@@ -8,19 +8,44 @@ class SearchGifView extends React.Component {
         super(props);
 
         this.handleClick = this.handleClick.bind(this);
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+        this.handleMouseOut = this.handleMouseOut.bind(this);
+
+        this.state = {
+            url: null
+        };
+    }
+    componentDidMount() {
+        let gif = this.props.gif;
+
+        this.setState({
+            url: gif.images.original_still.url
+        });
     }
     handleClick() {
         if (typeof this.props.click === 'function') {
             this.props.click(this.props.gif);
         }
     }
-    render() {
+    handleMouseOver() {
         let gif = this.props.gif;
 
+        this.setState({
+            url: gif.images.downsized_medium.url
+        });
+    }
+    handleMouseOut() {
+        let gif = this.props.gif;
+
+        this.setState({
+            url: gif.images.original_still.url
+        });
+    }
+    render() {
         return (
-            <div className="col-xs-4 search-gif-view">
+            <div className="col-xs-4 search-gif-view" onClick={this.handleClick} onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}>
                 <div className="thumbnail">
-                    <img src={gif.images.original_still.url} role="presentation"  onClick={this.handleClick} />
+                    <img role="presentation" src={this.state.url} />
                 </div>
             </div>
         );

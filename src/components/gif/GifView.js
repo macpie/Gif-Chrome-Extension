efaf567fca83beceece80f6d402e7d7185c48c5a
Peter Defebvre <macpie59@gmail.com>
2016-08-16 17:16:55 -0700

    Put data  in state

--- src/components/gif/GifView.js
+++ src/components/gif/GifView.js
@@ -6,8 +6,21 @@ import DownloadLink from '../common/DownloadLink';
 import '../../css/GifView.css'
 
 class GifView extends React.Component {
-    componentDidMount() {
+    constructor(props) {
+        super(props);
+
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleCopy = this.handleCopy.bind(this);
+
+        this.state = {};
+    }
+    componentWillMount() {
         var gif = this.props.gif;
+
+        this.setState(gif);
+    }
+    componentDidMount() {
+        var gif = this.state
 
         $('#' + gif.id).editable({
             selector: '.gif-name',
@@ -20,14 +33,14 @@ class GifView extends React.Component {
             }
         });
     }
-    handleDelete(id, e) {
-        GifActions.remove(id);
+    handleDelete() {
+        GifActions.remove(this.state.id);
     }
-    handleCopy(url) {
-        this.props.copy(url);
+    handleCopy() {
+        this.props.copy(this.state.url);
     }
     render() {
-        let gif = this.props.gif;
+        let gif = this.state;
 
         return (
             <div className="col-xs-4 gif-view" id={gif.id}>
@@ -35,10 +48,10 @@ class GifView extends React.Component {
                     <div className="caption">
                         <h3 className="gif-name">{gif.name}</h3>
                         <div className="btn-group btn-group-sm" role="group">
-                            <button type="button" className="btn btn-danger" onClick={this.handleDelete.bind(this, gif.id)}>
+                            <button type="button" className="btn btn-danger" onClick={this.handleDelete}>
                                 <i className="fa fa-trash-o" aria-hidden="true"></i>
                             </button>
-                            <button type="button" className="btn btn-success" onClick={this.handleCopy.bind(this, gif.url)}>
+                            <button type="button" className="btn btn-success" onClick={this.handleCopy}>
                                 <i className="fa fa-files-o" aria-hidden="true"></i>
                             </button>
                             <DownloadLink url={gif.url} />

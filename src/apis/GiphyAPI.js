cba21acbf1d4ac19e2e0178f0282c9a9120ef5f1
Peter Defebvre <macpie59@gmail.com>
2016-08-18 15:29:22 -0700

    Add giphy API upload

--- src/apis/GiphyAPI.js
+++ src/apis/GiphyAPI.js
@@ -5,12 +5,28 @@ import {GIPHY_API_KEY} from '../config';
 export const search = (req) => {
     return new Promise((resolve, reject) => {
         request
-            .get('http://api.giphy.com/v1/gifs/search')
+            .get('https://api.giphy.com/v1/gifs/search')
             .query({
                 api_key: GIPHY_API_KEY,
                 q: req.query,
                 limit: req.limit || 10,
                 offset: req.offset || 0
+            })
+            .end((err, res) => {
+                if (err) reject(err);
+                else resolve(res.body);
+            });
+    });
+};
+
+export const upload = (name, url) => {
+    return new Promise((resolve, reject) => {
+        request
+            .post('https://api.giphy.com/v1/gifs')
+            .send({
+                api_key: GIPHY_API_KEY,
+                source_image_url: url,
+                tags: name.split(' ').join(',')
             })
             .end((err, res) => {
                 if (err) reject(err);

ac625c842c07af589178213039712a0fd0c27cf7
Peter Defebvre <macpie59@gmail.com>
2016-08-19 11:25:30 -0700

    Fix giphy upload

--- src/apis/GiphyAPI.js
+++ src/apis/GiphyAPI.js
@@ -1,7 +1,8 @@
 import request from 'superagent';
 import Promise from 'promise';
 import {
-    GIPHY_API_KEY
+    GIPHY_SEARCH_API_KEY,
+    GIPHY_UPLOAD_API_KEY
 } from '../config';
 
 export const isGiphyUrl = (url) => {
@@ -19,9 +20,9 @@ export const getStillFromUrl = (url) => {
 export const search = (req) => {
     return new Promise((resolve, reject) => {
         request
-            .get('https://api.giphy.com/v1/gifs/search')
+            .get('http://api.giphy.com/v1/gifs/search')
             .query({
-                api_key: GIPHY_API_KEY,
+                api_key: GIPHY_SEARCH_API_KEY,
                 q: req.query,
                 limit: req.limit || 10,
                 offset: req.offset || 0
@@ -36,9 +37,9 @@ export const search = (req) => {
 export const upload = (name, url) => {
     return new Promise((resolve, reject) => {
         request
-            .post('https://api.giphy.com/v1/gifs')
-            .send({
-                api_key: GIPHY_API_KEY,
+            .post('http://api.giphy.com/v1/gifs')
+            .query({
+                api_key: GIPHY_UPLOAD_API_KEY,
                 source_image_url: url,
                 tags: name.split(' ').join(',')
             })

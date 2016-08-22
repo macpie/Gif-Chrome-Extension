f76fabc7d1101608341ebb44d313aaeb4dc53441
Peter Defebvre <macpie59@gmail.com>
2016-08-22 10:34:57 -0700

    Add basic context menu save gif

--- src/background.js
+++ src/background.js
@@ -1,1 +1,28 @@
-console.log('test');
+(function() {
+    var KEY = 'GIFS';
+
+    function getData() {
+        return JSON.parse(localStorage.getItem(KEY)) || [];
+    }
+
+    function clickHandler(info) {
+        var name = prompt("Name? "),
+            data = getData(),
+            gif = {
+                id: uuid.v4(),
+                name: name,
+                url: info.srcUrl,
+                still_url: info.srcUrl
+            };
+
+        data.push(gif);
+
+        localStorage.setItem(KEY, JSON.stringify(data));
+    }
+
+    chrome.contextMenus.create({
+        title: "Save Gif",
+        contexts: ["image"],
+        onclick: clickHandler
+    });
+})();

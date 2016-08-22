4cab946edb9e6feaba8ab3c31f6b8909fa0010c6
Peter Defebvre <macpie59@gmail.com>
2016-08-22 10:17:44 -0700

    Add default background page

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,15 @@ module.exports = function(grunt) {
                 src: ['*', '!powered_by.png'],
                 dest: 'build/static/media/'
             },
+            background: {
+                files: [{
+                    src: 'background.html',
+                    dest: 'build/background.html'
+                }, {
+                    src: 'src/background.js',
+                    dest: 'build/static/js/background.js'
+                }]
+            },
             manifest: {
                 src: 'manifest.json',
                 dest: 'build/manifest.json'

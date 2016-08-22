2c1e930d7b743b486b87c8354ad3ef1ce16dec3d
Peter Defebvre <macpie59@gmail.com>
2016-08-22 14:38:31 -0700

    Add migration

--- src/background.js
+++ src/background.js
@@ -1,6 +1,26 @@
 (function() {
     var KEY = 'GIFS';
 
+    // TO REMOVE @ SOME POINT
+    if (!localStorage.getItem('gifs_v2')) {
+        console.log('running migration');
+
+        chrome.storage.local.get('gifs_v2', function(data) {
+            var transformed = data.gifs_v2.map(function(obj) {
+                return {
+                    id: uuid.v4(),
+                    name: obj.name,
+                    url: obj.imgur_url || obj.url,
+                    still_url: obj.imgur_square
+                };
+            });
+            localStorage.setItem(KEY, JSON.stringify(transformed));
+
+            localStorage.setItem('gifs_v2', new Date());
+            console.log('migration done');
+        });
+    }
+
     function getData() {
         return JSON.parse(localStorage.getItem(KEY)) || [];
     }
